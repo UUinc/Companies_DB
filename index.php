76edcd481a4556671da0fa4fa71876a290feeb93
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2022-08-04 11:07:07 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!Doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css" />
+        <title>Companies DB</title>
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <h1><b>Companies Database</b></h1>
+                <select name="cities">
+                    <option>Rabat</option>
+                    <option>Casablanca</option>
+                    <option>Marrakech</option>
+                </select> 
+                <select name="services">
+                    <option>Development</option>
+                    <option>Food</option>
+                    <option>Delivery</option>
+                </select> 
+                <br/>
+                <textarea name="message" placeholder="message here..." cols="50" rows="10"></textarea>
+                <br/>
+                <button name="send" type="submit">send</button>
+            </div>
+        </div>
+    </body>
+</html>
